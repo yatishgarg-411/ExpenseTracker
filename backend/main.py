d03e05f18f487ddac27309883c73749c876889ee
yatishgarg-411 <126184526+yatishgarg-411@users.noreply.github.com>
2025-06-28 00:42:46 +0530

    Fix CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,18 @@ from fastapi.middleware.cors import CORSMiddleware
 from router.user import router as user_router
 from router.transaction import router as transaction_router
 
-app=FastAPI()
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://expense-tracker-indol-theta-22.vercel.app",
+    "https://expense-tracker-git-deploy-v1-yatish-gargs-projects.vercel.app",
+    "https://expense-tracker-yatish-gargs-projects.vercel.app"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
