e955db8e7fbfdaa327bd4d653835301b2503a0dd
yatishgarg-411 <126184526+yatishgarg-411@users.noreply.github.com>
2025-06-28 00:21:23 +0530

    CORS middleware allowed for vercel

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['http://localhost:3000', 'https://expense-tracker-q4vk.vercel.app/'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
